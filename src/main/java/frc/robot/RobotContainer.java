10bdb86fa8c5bace3b89cc3bc90c38f059c80569
Your Name <you@example.com>
2025-02-18 07:54:41 -0500

    switched swerve default to robotcentric

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -56,7 +56,7 @@ public class RobotContainer {
                 () -> -driver.getRawAxis(translationAxis), 
                 () -> -driver.getRawAxis(strafeAxis), 
                 () -> -driver.getRawAxis(rotationAxis), 
-                () -> robotCentric.getAsBoolean()
+                () -> !robotCentric.getAsBoolean()
             )
         );
 
